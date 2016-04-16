00693828ac5a933d5fedf289c5384cecff2ad3de
Bryan Cheng <cycheng@yaho-inc.com>
2016-04-16 22:53:52 +0800

    init

---
+++ index.js
@@ -0,0 +1,43 @@
+var express = require('express');
+var flickr = require('./routes/flickr');
+var search = require('./routes/search');
+var app = express();
+var bodyParser = require('body-parser');
+var port = process.env.PORT || 8080;
+var router = express.Router();
+var helmet = require('helmet');
+var cors = require('cors');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(helmet());
+
+/* root */
+router.get('/', function(req, res) {
+    res.json({ message: 'bryanyuan2 heroku API' });
+});
+
+/* flickr getFavPhotos */
+router.get('/flickr/fav', flickr.getFavPhotos);
+
+/* search api/media */
+router.get('/search/query/:id', search.test);
+
+/* */
+var whitelist = [
+    'http://localhost:3000',
+];
+var corsOptions = {
+    origin: function(origin, callback){
+        var originIsWhitelisted = whitelist.indexOf(origin) !== -1;
+        callback(null, originIsWhitelisted);
+    },
+    credentials: true
+};
+app.use(cors(corsOptions));
+/* */
+
+app.use('/api', router);
+
+app.listen(port);
+console.log('port ' + port);
---
+++ routes/search.js
@@ -0,0 +1,46 @@
+var _ = require("lodash");
+var Promise = require('promise');
+var Flickr = require("flickrapi");
+var elasticsearch = require('elasticsearch');
+
+var client = new elasticsearch.Client({
+  host: 'localhost:9200'
+  //log: 'trace'
+});
+
+var searchModel = {};
+
+function searchMediaQuery(conf) {
+    return new Promise(function (resolve, reject) {
+        client.search({
+          index: conf.index,
+          type: conf.type,
+          q: conf.query,
+          size: conf.limit
+        }).then(function (body) {
+          var hits = body.hits.hits;
+          //console.log("hits", hits);
+          resolve(hits);
+        }, function (error) {
+          //console.trace(error.message);
+        });
+    });
+}
+
+searchModel.test = function(req, res) {
+    var output = res;
+    var conf = {
+        index: 'api',
+        type: 'media',
+        query: req.params.id || 'yahoo',
+        limit: req.param('limit') || 5
+    };
+
+    searchMediaQuery(conf).then(function(res){
+        var json = JSON.parse(JSON.stringify(res));
+        //res.send([{name:'wine1'}, {name:'wine2'}, {name:'wine3'}]);
+        output.send(json);
+    });
+};
+
+module.exports = searchModel;
