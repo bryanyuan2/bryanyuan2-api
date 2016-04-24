6c7f863dcb830351908fbc6a7eb08c1eaa6f3ff5
Bryan Cheng <cycheng@yaho-inc.com>
2016-04-24 20:19:13 +0800

    added

--- routes/search.js
+++ routes/search.js
@@ -2,14 +2,10 @@ var _ = require("lodash");
 var Promise = require('promise');
 var Flickr = require("flickrapi");
 var elasticsearch = require('elasticsearch');
-var AgentKeepAlive = require('agentkeepalive');
 
 var client = new elasticsearch.Client({
   apiVersion: '2.1',
-  host: 'http://localhost:9200',
-  createNodeAgent(connection, config) {
-    return new AgentKeepAlive(connection.makeAgentConfig(config));
-  }
+  host: 'http://127.0.0.1:9200'
 });
 
 var searchModel = {};
