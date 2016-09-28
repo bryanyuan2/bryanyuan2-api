9a4338de07e78f80cefa1ee99d7855549bf2c0f9
Bryan Cheng <cycheng@yahoo-inc.com>
2016-09-29 02:17:38 +0800

    updated ssh via 8081

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
 var express = require('express'),
     helmet = require('helmet'),
     cors = require('cors'),
-    bodyParser = require('body-parser');
+    bodyParser = require('body-parser'),
+    fs = require('fs'),
+    https = require('https');
 
 var index = require('./routes/index'),
     search = require('./routes/search'),
@@ -9,12 +11,18 @@ var index = require('./routes/index'),
     instapaper = require('./routes/instapaper');
 
 var app = express(),
-    route = express.Router();
+    route = express.Router(),
+    server = https.createServer({
+        key: fs.readFileSync('./tls/key.pem'),
+        cert: fs.readFileSync('./tls/cert.pem')
+    }, app);
 
 var whitelist = [
         'http://localhost:3000',
         'http://bryanyuan2.github.io',
+        'https://bryanyuan2.github.io',
         'http://bryanyuan2-bookmarks.github.io',
+        'https://bryanyuan2-bookmarks.github.io',
     ],
     corsOptions = {
         origin: function(origin, callback){
@@ -23,7 +31,7 @@ var whitelist = [
         },
         credentials: true
     },
-    port = 8080;
+    ports = [8080, 8081];
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
@@ -40,7 +48,9 @@ route.get('/novel/actor/:gid', novel.getActorRelation);
 route.get('/instapaper/get', instapaper.getList);
 app.use('/api', route);
 
-app.listen(port);
-console.log('port ' + port);
+app.listen(ports[0]);
+server.listen(ports[1])
+
+console.log('port ' + ports);
 
 
